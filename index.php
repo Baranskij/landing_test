18cc58c21bc10213d466f8ddd0b859c009a6b689
Baranskij <82298610+Baranskij@users.noreply.github.com>
2021-05-20 19:08:12 +0700

    add
    add project

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SemTech</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body class="big_body">
+    <div class="centr wrap">
+        <header class="headed">
+            <h1 class="heading">SemTech IT Solutions is all <br> about <span class="Customer">Customer</span> <span
+                    class="Experience">Experience.</span></h1>
+            <img class="logo" src="img/logo_297.png" alt="logo">
+            <p class="text">This is something we hold near and dear to us. <br>
+                Please share your experience with SemTech IT Solutions.</p>
+        </header>
+
+        <section class="wrapper">
+            <div class="btn_box">
+                <button class="btn like">I Had A Great Experience</button>
+                <button class="btn dlike">I Had A Bad Experience</button>
+            </div>
+        </section>
+
+    </div>
+
+</body>
+
+</html>
